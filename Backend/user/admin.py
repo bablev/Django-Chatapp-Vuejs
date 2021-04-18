d609c4563927a768710586ec75a14a8f137c2eb8
Evgeniy <zhenyafarter@gmail.com>
2021-04-18 21:52:09 +0300

    Custom user

--- Backend/user/admin.py
+++ Backend/user/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from user.forms import CustomUserCreationForm, CustomUserChangeForm
+from user.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email']
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
--- Backend/user/models.py
+++ Backend/user/models.py
@@ -9,7 +9,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError('Email must be set')
 
         email = self.normalize_email(email)
-        user = self.model(email, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
@@ -27,12 +27,12 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractUser):
+    username = None
     email = models.EmailField(unique=True, verbose_name='Почта')
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    objects = CustomUserManager
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
